bf34b9a4e05ab814d6cda1074f5770841790dd87
mahat <mehmet.ahat@gmail.com>
2016-11-23 02:24:55 +0300

    example datasets and simple model

---
+++ SimplePoissonRegression.py
@@ -0,0 +1,58 @@
+'''
+Author: mahat
+'''
+
+import pandas as pd
+import statsmodels.api as sm
+import numpy as np
+import matplotlib.pyplot as plt
+
+# read data
+from statsmodels.genmod.families import Poisson
+from statsmodels.genmod.cov_struct import Independence
+from statsmodels.genmod.generalized_estimating_equations import GEE
+
+data = pd.read_csv('./data/StudentData.csv', delimiter=',',header=0)
+
+print '----- data head -----'
+print data.head()
+print '----- data description -----'
+print data.describe()
+histData = []
+uniqProgs = sorted(data['prog'].unique())
+for elem in uniqProgs:
+    histData.append(data[data['prog'] == elem]['num_awards'].values)
+
+# plotting histogram in order to see
+plt.hist(tuple(histData),bins=10, normed=True,histtype='bar',label= map(lambda x: 'Prog '+ str(x),uniqProgs))
+plt.legend()
+plt.ylabel('Count')
+plt.title('Histogram for each program')
+plt.show()
+
+# adding dummy variables in order to handle categorical data in prog
+prog_dummies = pd.get_dummies(data['prog']).rename(columns=lambda x: 'prog_' + str(x))
+dataWithDummies = pd.concat([data, prog_dummies], axis=1)
+dataWithDummies .drop(['prog', 'prog_3'], inplace=True, axis=1)
+dataWithDummies = dataWithDummies .applymap(np.int)
+
+print dataWithDummies.head()
+
+# applying possion regression on data
+# assuming variables are independent to each other
+feat_cols = ['math', 'prog_1', 'prog_2']
+X = [elem for elem in dataWithDummies[feat_cols].values]
+X = sm.add_constant(X, prepend=False)
+Y = [elem for elem in dataWithDummies['num_awards'].values]
+
+poisson_mod = sm.Poisson(Y, X)
+poisson_res = poisson_mod.fit(method="newton")
+print(poisson_res.summary())
+
+
+predVals = poisson_res.predict(X)
+
+plt.plot(range(len(Y)), Y, 'r*-', range(len(Y)), predVals, 'bo-')
+plt.title('Train dataset Real vs. Predicted Values')
+plt.legend(['Real Values', 'Predicted Values'])
+plt.show()
